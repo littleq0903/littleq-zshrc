1821fd858bd9298111caf0d39343fb0b0fecbf88
Colin Su <littleq0903@gmail.com>
2019-06-24 00:31:48 -0700

    use realpath 2

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 git submodule update --init
 
-ln -s `dirname `realpath $0``/.zshrc $HOME/.zshrc
-ln -s `dirname `realpath $0``/.virtualenv.zsh $HOME/.virtualenv.zsh
+ln -s $(dirname $(realpath $0))/.zshrc $HOME/.zshrc
+ln -s $(dirname $(realpath $0))/.virtualenv.zsh $HOME/.virtualenv.zsh
